23f60ebdc772baa0c04ba5d97fd9fc3b9e1d84d9
Sergey Burmisov <burmisov@informatica.ru>
2014-07-25 12:03:29 +0400

    some progress :)

--- routes.js
+++ routes.js
@@ -1,5 +1,19 @@
+var validRoutes = ['', 'second', 'third'];
+
 module.exports = function (app) {
 	app.get('/', function (req, res) {
 		res.render('index')
 	});
-}
+
+	app.get('/:route', function (req, res) {
+		if (validRoutes.indexOf(req.params.route) > -1) {
+			res.render('index');
+		} else {
+			res.send(404, 'not found.');
+		}
+	});
+
+	app.get('/partials/:partial', function (req, res) {
+		res.render('partials/' + req.params.partial);
+	});
+};
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,35 @@
-angular.module('ngRoutingExample', []);
+angular.module('ngRoutingExample', ['ngRoute'])
+
+	.controller('MainController', function ($scope, $route, $routeParams, $location) {
+
+	})
+
+	.controller('FirstController', function ($scope) {
+
+	})
+
+	.controller('SecondController', function ($scope) {
+
+	})
+
+	.controller('ThirdController', function ($scope) {
+
+	})
+
+	.config(function ($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'partials/first',
+				controller: 'FirstController'
+			})
+			.when('/second', {
+				templateUrl: 'partials/second',
+				controller: 'SecondController'
+			})
+			.when('/third', {
+				templateUrl: 'partials/third',
+				controller: 'ThirdController'
+			});
+
+		$locationProvider.html5Mode(true);
+	});
