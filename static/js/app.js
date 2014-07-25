0656c9145cf68d1da1034aea77b61d4bd781fa2b
Sergey Burmisov <burmisov@informatica.ru>
2014-07-25 17:13:49 +0400

    implement noReload directive

--- static/js/app.js
+++ static/js/app.js
@@ -9,11 +9,27 @@ angular.module('ngRoutingExample', ['ngRoute'])
 	})
 
 	.controller('SecondController', function () {
+		console.log('SecondController started.');
+
 		// todo
 	})
 
 	.controller('ThirdController', function () {
 		// todo
+	})
+
+	.directive('noReload', function ($location, $anchorScroll) {
+		return {
+			link: function (scope, elem, attrs) {
+				elem.on('click', function (e) {
+					e.preventDefault();
+					var hash = /#.*$/.exec(attrs.href)[0].slice(1);
+					$location.hash(hash);
+					location.hash = hash;
+					$anchorScroll();					
+				});
+			}
+		}
 	})
 
 	.config(function ($routeProvider, $locationProvider) {
