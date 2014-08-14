f774a56e7458438663ef2df1f9cf66a6104971a1
Sergey Burmisov <burmisov@informatica.ru>
2014-08-14 18:23:57 +0400

    добавление тестов на переходы по хэш-ссылкам

--- test/client/routing.spec.js
+++ test/client/routing.spec.js
@@ -27,4 +27,42 @@ describe('Маршрутизатор на базе ng-route', function () {
 		var rootHeaderAfterRefresh = element(by.css('div[ng-view]')).element(by.tagName('h3'));
 		expect(rootHeaderAfterRefresh.getText()).toContain('second template');
 	});
+
+	it('должен открывать маршрут при щелчке по ссылке', function () {
+		browser.get('/');
+		var linkToSecond = element(by.linkText('Second view'));
+		linkToSecond.click();
+		var rootHeader = element(by.css('div[ng-view]')).element(by.tagName('h3'));
+		expect(rootHeader.getText()).toContain('second template');
+	});
+
+	it('должен позволять скролл при щелчке на хэш-ссылку', function (done) {
+		browser.get('/second');
+		var getPositionScript = "return document.body.scrollTop";
+		browser.executeScript(getPositionScript).then(function (scrollPosBefore) {
+			var linkToSecondHash = element(by.linkText('Dolor sit amet'));
+			linkToSecondHash.click();
+			browser.executeScript(getPositionScript).then(function (scrollPosAfter) {
+				expect(scrollPosAfter).toBeGreaterThan(scrollPosBefore);
+				done();
+			});
+		});
+	});
+
+	it('должен осуществлять скролл при прямом переходе на хэш-ссылку', function (done) {
+		browser.get('/second');
+		var getPositionScript = "return document.body.scrollTop";
+		browser.executeScript(getPositionScript).then(function (scrollPosBefore) {
+			var linkToSecondHash = element(by.linkText('Dolor sit amet'));
+			linkToSecondHash.click();
+			browser.executeScript(getPositionScript).then(function (scrollPosAfter) {
+				expect(scrollPosAfter).toBeGreaterThan(scrollPosBefore);
+				browser.get('/second#dolorsitamet');
+				browser.executeScript(getPositionScript).then(function (scrollPosDirect) {
+					expect(scrollPosDirect).toEqual(scrollPosBefore);
+					done();
+				});				
+			});
+		});
+	});
 });
