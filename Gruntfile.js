3a611b21505130d8ff92ed9e099ffefbaa89fbd0
Sergey Burmisov <burmisov@informatica.ru>
2014-08-14 16:51:23 +0400

    добавление запуска тестов protractor через grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,29 @@ module.exports = function (grunt) {
 		},
 		jasmine_node: {
 			all: ['test/server/']
+		},
+		protractor: {
+			local: {
+				options: {
+					configFile: 'test/client/conf-local.js'
+				}
+			}
+		},
+		exec: {
+			installChromeDriver: {
+				cmd: 'node .\\node_modules\\grunt-protractor-runner\\node_modules\\protractor' +
+					'\\bin\\webdriver-manager update --chrome'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-jasmine-node');
+	grunt.loadNpmTasks('grunt-protractor-runner');
+	grunt.loadNpmTasks('grunt-exec');
 
 	grunt.registerTask('lint', ['jshint']);
 	grunt.registerTask('unit-test', ['jasmine_node']);
 	grunt.registerTask('default', ['jshint', 'jasmine_node']);
+	grunt.registerTask('test-protractor', ['exec:installChromeDriver', 'protractor']);
 };
